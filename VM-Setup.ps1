265462574efbfaaabe50088cd423ca1ccbe6c24e
kalen1c <138606076+kalen1c@users.noreply.github.com>
2026-07-08 12:07:21 +1000

    Add VM setup script for honeypot environment
    This script sets up prerequisites for the honeypot VM environment, including NTP synchronization, Az.KeyVault module installation, and IMDS IP access blocking.

---
+++ VM-Setup.ps1
@@ -0,0 +1,24 @@
+## This script establishes the necessary prerequisites for the honeypot VM environment
+
+Write-Host "Synchronizing system clock with global NTP servers..."
+# Stop the time service to release any locked files
+Stop-Service w32time
+# Configure the VM to sync with the global NTP pool
+w32tm /config /manualpeerlist:"pool.ntp.org,0x8" /syncfromflags:manual /update
+# Restart the service and force an immediate resync
+Start-Service w32time
+w32tm /resync
+
+# Installs Az.KeyVault Module
+Write-Host "Installing Az.KeyVault module..."
+Install-Module -Name Az.KeyVault -Force -AllowClobber -Scope AllUsers
+
+Write-Host "Blocking user access to IMDS IP..."
+# Grab the SID of user
+$UserSID = (Get-LocalUser -Name "SentinelAzure").SID.Value
+# Format the SID into the required SDDL string format
+$SDDL = "D:(A;;CC;;;$UserSID)"
+# Apply the rule
+New-NetFirewallRule -DisplayName "IMDS Lockdown"  -Direction Outbound -Action Block -RemoteAddress 169.254.169.254 -LocalUser $SDDL
+
+Write-Host "User setup Complete!"
